ef8ffc51480222799f08fba8137560abf0a32bd8
Polatcan Gürbüz <427638@ogr.ktu.edu.tr>
2024-08-09 16:22:47 +0300

    hugging face api speech to text added

---
+++ Assets/Scripts/SpeechToText/SpeechRecognition.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+using HuggingFace.API;
+public class SpeechRecognition : MonoBehaviour
+{
+    [SerializeField] Button startButton;
+    [SerializeField]Button stopButton;
+    [SerializeField] TextMeshProUGUI text;
+
+
+    AudioClip clip;
+    byte[] bytes;
+    bool recording;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartRecording);
+        stopButton.onClick.AddListener(StopRecording);
+    }
+    private void SendRecording()
+    {
+        HuggingFaceAPI.AutomaticSpeechRecognition(bytes, response => {
+            text.color = Color.white;
+            text.text = response;
+        }, error => {
+            text.color = Color.red;
+            text.text = error;
+        });
+    }
+    void StartRecording()
+    {
+        clip = Microphone.Start(null, false, 10, 44100);
+        recording = true;
+    }
+
+    private void Update()
+    {
+        if(recording && Microphone.GetPosition(null) >= clip.samples)
+        {
+            StopRecording();
+        }
+    }
+
+    void StopRecording()
+    {
+        var position = Microphone.GetPosition(null);
+        Microphone.End(null);
+        var samples = new float[position * clip.channels];
+        clip.GetData(samples, 0);
+        bytes = EncodeAsWAV(samples,clip.frequency,clip.channels);
+        recording = false;
+        SendRecording();
+    }
+
+    private byte[] EncodeAsWAV(float[] samples, int frequency, int channels)
+    {
+        using (var memoryStream = new MemoryStream(44 + samples.Length * 2))
+        {
+            using (var writer = new BinaryWriter(memoryStream))
+            {
+                writer.Write("RIFF".ToCharArray());
+                writer.Write(36 + samples.Length * 2);
+                writer.Write("WAVE".ToCharArray());
+                writer.Write("fmt ".ToCharArray());
+                writer.Write(16);
+                writer.Write((ushort)1);
+                writer.Write((ushort)channels);
+                writer.Write(frequency);
+                writer.Write(frequency * channels * 2);
+                writer.Write((ushort)(channels * 2));
+                writer.Write((ushort)16);
+                writer.Write("data".ToCharArray());
+                writer.Write(samples.Length * 2);
+
+                foreach (var sample in samples)
+                {
+                    writer.Write((short)(sample * short.MaxValue));
+                }
+            }
+            return memoryStream.ToArray();
+        }
+    }
+
+}
