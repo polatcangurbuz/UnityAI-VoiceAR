863d89de91c5df7323d04354bcdaa561b38dbdf5
Polatcan Gürbüz <427638@ogr.ktu.edu.tr>
2024-08-09 11:36:45 +0300

    Character and character mouth script added

---
+++ Assets/Scripts/ScaleFromMicrophone.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleFromMicrophone : MonoBehaviour
+{
+    public AudioSource source;
+    public Vector3 minScale;
+    public Vector3 maxScale;
+    public AudioLoudnessDetection detector;
+
+
+    public float loudnessSensibility = 100;
+    public float threshold = 0.1f;
+    private void Update()
+    {
+        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
+
+        if (loudness < threshold)
+            loudness = 0;
+
+        transform.localScale = Vector3.Lerp(minScale, maxScale, loudness);
+    }
+
+
+
+
+}
